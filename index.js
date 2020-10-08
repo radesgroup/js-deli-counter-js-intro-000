395e1997f3eb449d2c784a7d2db9eff90852b278
Aleksey <fduchunru@gmail.com>
2020-10-08 13:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ function currentLine(line){
   if(!line.length){
     return "The line is currently empty."
   }
+
+  const numAndName = [];
+
+   for(let i = 0; i = line.length; i++){
+     numAndName.push('${i + 1}. ${line[i]}')
+   }
+   return "The line is currently: ${numAndName.join(', ')}";
+   
 }
